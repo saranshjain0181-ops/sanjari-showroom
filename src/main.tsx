a223e0ceaea02c8441b5082d9c58335ea4793f8e
saranshjain0181-ops <saranshjain0181@gmail.com>
2026-02-01 19:18:27 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+console.log("MY KEY IS:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 createRoot(document.getElementById("root")!).render(<App />);
